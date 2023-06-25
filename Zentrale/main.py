9d9b94e96af0c2239a7a62a5e5f2c9e562ed9169
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-06-25 13:21:44 +0200

    vs code python settings

--- Zentrale/main.py
+++ Zentrale/main.py
@@ -3,12 +3,21 @@ import paho.mqtt.client as mqtt
 
 def main():
     broker_address = "192.168.178.11"
-    pass
 
+    # client instanzieren
+    client = mqtt.Client("master")
+
+    # verbindung aufbauen
+    client.connect(broker_address)
+
+    # subscribe topic
+    client.subscribe("house/main")
+
+    # sende nachricht
+    client.publish("house/main", "Die Zentrale ist online")
 
 
 if __name__ == '__main__':
     main()
 
 
-    
