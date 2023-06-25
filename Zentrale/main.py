c7d182090a21b52a0921fb650696941d6e7b8ac4
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-06-25 13:09:32 +0200

    clean code init broker-address

--- Zentrale/main.py
+++ Zentrale/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import paho.mqtt.client as mqtt
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    broker_address = "192.168.178.11"
+    pass
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    
