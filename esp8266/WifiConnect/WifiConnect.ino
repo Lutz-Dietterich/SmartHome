0cc0f2d0ff6f66a23fc5b725f8cf0a7144780259
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-06-25 20:39:57 +0200

    esp wifi connecting

---
+++ esp8266/WifiConnect/WifiConnect.ino
@@ -0,0 +1,29 @@
+
+#include <ESP8266WiFi.h>
+
+const char *WIFI_SSID = "FRITZ!Box 7530 VF";
+const char *WIFI_PASSWORD = "54084965601593359625";
+
+
+WiFiClient client;
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Verbindungsaufbau zum WLAN"); 
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(WiFi.localIP());
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
